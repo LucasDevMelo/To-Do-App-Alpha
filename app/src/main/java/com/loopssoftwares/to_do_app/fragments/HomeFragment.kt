cea0316773f820f05958b10550a44d07947c260d
Lucas Lopes <110427652+LucasDevMelo@users.noreply.github.com>
2023-10-05 17:38:01 -0300

    Atualização dia 05/10/2023 - Tasks on HomeFragment

--- app/src/main/java/com/loopssoftwares/to_do_app/fragments/HomeFragment.kt
+++ app/src/main/java/com/loopssoftwares/to_do_app/fragments/HomeFragment.kt
@@ -8,20 +8,26 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.loopssoftwares.to_do_app.R
 import com.loopssoftwares.to_do_app.databinding.FragmentHomeBinding
+import com.loopssoftwares.to_do_app.utils.ToDoAdapter
+import com.loopssoftwares.to_do_app.utils.ToDoData
 
-class HomeFragment : Fragment(), AddToDoPopUpFragment.DialogNextBtnClickListener {
+class HomeFragment : Fragment(), AddToDoPopUpFragment.DialogNextBtnClickListener,
+    ToDoAdapter.ToDoAdapterClicksInterface {
 
     private lateinit var auth : FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding: FragmentHomeBinding
     private lateinit var popUpFragment : AddToDoPopUpFragment
+    private lateinit var adapter: ToDoAdapter
+    private lateinit var mList: MutableList<ToDoData>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,6 +41,7 @@ class HomeFragment : Fragment(), AddToDoPopUpFragment.DialogNextBtnClickListener
         super.onViewCreated(view, savedInstanceState)
 
         init(view)
+        getDataFromFirebase()
         registerEvents()
     }
 
@@ -53,6 +60,36 @@ class HomeFragment : Fragment(), AddToDoPopUpFragment.DialogNextBtnClickListener
         navController = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference.child("Tarefas").child(auth.currentUser?.uid.toString())
+
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        mList = mutableListOf()
+        adapter = ToDoAdapter(mList)
+        adapter.setListener(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase(){
+        databaseRef.addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for(taskSnapshot in snapshot.children){
+                    val todoTask = taskSnapshot.key?.let{
+                        ToDoData(it , taskSnapshot.value.toString())
+                    }
+
+                    if (todoTask != null){
+                        mList.add(todoTask)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context , error.message , Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     override fun onSaveTask(todo: String, todoEt: TextInputEditText) {
@@ -67,6 +104,20 @@ class HomeFragment : Fragment(), AddToDoPopUpFragment.DialogNextBtnClickListener
 
             popUpFragment.dismiss()
         }
+    }
+
+    override fun onDeleteBtnClicked(toDoData: ToDoData) {
+        databaseRef.child((toDoData.taskId)).removeValue().addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(context , "Tarefa deletada com sucesso!" , Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context , it.exception?.message , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onEditTaskBtnClicked(toDoData: ToDoData) {
+        TODO("Not yet implemented")
     }
 
 }
